b155934746ca0178fcbedfb42c5a8789cce94026
Roberto S. Ramos Jr <robertosrjr@gmail.com>
2021-07-04 22:38:27 -0300

    new version

--- src/main/java/dev/robertosrjr/GreetingResource.java
+++ src/main/java/dev/robertosrjr/GreetingResource.java
@@ -1,4 +1,4 @@
-package com.gmail.robertosrjr.quarkus;
+package dev.robertosrjr;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -6,11 +6,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
-public class HelloResource {
+public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Olá";
+        return "Hello RESTEasy";
     }
 }
